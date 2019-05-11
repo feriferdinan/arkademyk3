12c62ea460eb62cae16a1cb1216fcd0d43680915
Septian Feri Ferdinan <50045891+feriferdinan@users.noreply.github.com>
2019-05-11 20:51:30 +0700

    Update 5.php

--- 5.php
+++ 5.php
@@ -1,16 +1,15 @@
 <?php
-			function sort_array($array){
-			sort($array);
-			$arr = [];
-			foreach($array as $a){
-					sort($a);
-					$arr[] = $a;
-			}
-			return $arr;
-			}
-			$array = [
-					["g","h","i","j"],
-					["a","c","b","e","d"],
-					["g","e","f"]
-					];
-			print_r(sort_array($array));
+
+$array = [["e","g"],["x","e","w"]];
+	function sort_array($array){
+	sort($array);
+	$arr = [];
+	foreach($array as $ar){
+		sort($ar);
+		$arr[] = $ar;
+	}
+	return $arr;
+}
+print_r(sort_array($array));
+
+?>
